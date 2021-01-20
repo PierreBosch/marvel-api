a4b0dcb63cd4fc4618b8c84f4e949e601d9313e8
Pierre Boschetto <pierre.mota@gam.com.br>
2021-01-20 14:15:41 -0300

    Create Authentication method

---
+++ src/app/controllers/SessionController.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+
+
+class SessionController {
+
+    async authenticate(req: Request, res: Response) {
+        const repository = getRepository(User)
+        const { email, password } = req.body;
+
+        const user = await repository.findOne({ where: { email }});
+
+        if(!user) {
+            return res.sendStatus(401);
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if(!isValidPassword) {
+            return res.sendStatus(401);
+        }
+
+        const token = jwt.sign({ id: user.id }, '@secret', { expiresIn: '1d'});
+
+        delete user.password;
+
+        return res.json({
+            user,
+            token
+        });
+
+    }
+
+}
+
+export default new SessionController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const router = Router();
 
+router.post('/users', UserController.store);
+router.post('/session', SessionController.authenticate);
 
 export default router;
