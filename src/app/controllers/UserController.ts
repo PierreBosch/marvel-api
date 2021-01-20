edbfc4f99b4342647dd38f0454fbad37a1fa4528
Pierre Boschetto <pierre.mota@gam.com.br>
2021-01-20 14:26:48 -0300

    Create middleware Authentication

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -4,6 +4,10 @@ import User from "../models/User";
 
 class UserController {
 
+    async index(req: Request, res: Response) {
+        return res.json({ userId: req.userId })
+    }
+
     async store(req: Request, res: Response) {
         const repository = getRepository(User)
         const { email, password } = req.body;
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+
+import authMiddleware from './app/middlewares/authMiddleware';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
@@ -6,5 +8,6 @@ const router = Router();
 
 router.post('/users', UserController.store);
 router.post('/session', SessionController.authenticate);
+router.get('/users', authMiddleware, UserController.index);
 
 export default router;
