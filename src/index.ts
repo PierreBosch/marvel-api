584e76befbe65a1539cbc2efd9f73421da76108b
Pierre Boschetto <pierre.mota@gam.com.br>
2021-01-21 23:28:56 -0300

    Added CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes'
+import cors from 'cors';
 
 import './database/connect';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
